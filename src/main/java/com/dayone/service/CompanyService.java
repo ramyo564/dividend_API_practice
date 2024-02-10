3f3137d7a28274df64f01dbb4da9d9c625ee9086
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-02-10 17:07:23 +0900

    트라이 자료구조 -> 자동검색 -> 싱글톤 빈으로 공유 관리

--- src/main/java/com/dayone/service/CompanyService.java
+++ src/main/java/com/dayone/service/CompanyService.java
@@ -8,6 +8,7 @@ import com.dayone.persist.entity.CompanyEntity;
 import com.dayone.persist.entity.DividendEntity;
 import com.dayone.scraper.Scraper;
 import lombok.AllArgsConstructor;
+import org.apache.commons.collections4.Trie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class CompanyService {
 
+    private final Trie trie;
     private final Scraper yahooFinanceScraper;
 
     private final CompanyRepository companyRepository;
@@ -28,23 +30,29 @@ public class CompanyService {
     public Company save(String ticker){
         boolean exists = this.companyRepository.existsByTicker(ticker);
         if (exists) {
-            throw new RuntimeException("already exists ticker - > " + ticker);
+            throw new RuntimeException(
+                    "already exists ticker - > " + ticker);
         }
         return this.storeCompanyAndDividend(ticker);
     }
 
     private Company storeCompanyAndDividend(String ticker){
         // ticker 를 기준으로 회사를 스크래핑
-        Company company = this.yahooFinanceScraper.scrapCompanyByTicker(ticker);
+        Company company =
+                this.yahooFinanceScraper.scrapCompanyByTicker(ticker);
         if (ObjectUtils.isEmpty(company)){
-            throw new RuntimeException("failed to scrap ticker -> " + ticker);
+            throw new RuntimeException(
+                    "failed to scrap ticker -> " + ticker);
         }
         // 해당 회사가 존재할 경우, 회사의 배당금 정보를 스크래핑
-        ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(company);
+        ScrapedResult scrapedResult =
+                this.yahooFinanceScraper.scrap(company);
 
         // 스크래핑 결과
-        CompanyEntity companyEntity = this.companyRepository.save(new CompanyEntity(company));
-        List<DividendEntity> dividendEntities = scrapedResult.getDividends().stream()
+        CompanyEntity companyEntity =
+                this.companyRepository.save(new CompanyEntity(company));
+        List<DividendEntity> dividendEntities =
+                scrapedResult.getDividends().stream()
                 .map(e -> new DividendEntity(companyEntity.getId(), e))
                 .collect(Collectors.toList());
 
@@ -57,4 +65,9 @@ public class CompanyService {
     public Page<CompanyEntity> getAllCompany(Pageable pageable){
         return this.companyRepository.findAll(pageable);
     }
+
+    public void addAutoCompleteKeyword(String keyword){
+        this.trie.put(keyword, null);
+    }
+
 }
