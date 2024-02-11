c809925f5b2d83f24b0e41b6b0055645cb3d096b
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-02-11 18:35:38 +0900

    회원가입 및 로그인 컨트롤러 간단한 구현

--- src/main/java/com/dayone/web/AutoController.java
+++ src/main/java/com/dayone/web/AutoController.java
@@ -1,4 +1,41 @@
 package com.dayone.web;
 
+import com.dayone.model.Auth;
+import com.dayone.security.TokenProvider;
+import com.dayone.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AutoController {
+
+    private final MemberService memberService;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp requset){
+        // 회원가입을 위한 API
+        return null;
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody Auth.SignIn request){
+        // 로그인을 위한 API
+        var member =
+                this.memberService.authenticate(request);
+        var token =
+                this.tokenProvider.generateToken(
+                        member.getUsername(),
+                        member.getRoles());
+        
+        return ResponseEntity.ok(token);
+    }
 }
